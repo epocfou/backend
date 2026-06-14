fb628c51dfe90f039e849be9fea17d187f6fc5db
Jaideep Singh <epocfou@gmail.com>
2026-06-14 22:47:18 +0530

    express.js quick start

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello world")
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/a", (req, res) => {
+  res.send("Hello World1!");
+});
+
+app.get("/b", (req, res) => {
+  res.send("Hello World!2");
+});
+
+app.get("/c", (req, res) => {
+  res.send("Hello World!3");
+});
+
+app.get("/d", (req, res) => {
+  res.send("Hello World!4");
+});
+
+app.get("/e", (req, res) => {
+  res.send("Hello World!5");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
